04233a236180d9018d7fe249d5858122390f5167
rachele <rachele.terzoli@gmail.com>
2020-03-18 17:03:29 +0100

    esercizio finito

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// creare variabile nome
+var nome;
+// creare variabile cognome
+var cognome;
+// creare variabile colore preferito
+var colorepreferito;
+// creare variabile password
+var password=nome;
+// chiedere nome
+nome = prompt("Inserisci il tuo nome");
+
+// chiedere cognome
+cognome = prompt("Inserisci il tuo cognome");
+
+// chiedere colore preferito
+colorepreferito = prompt("Inserisci il tuo colore preferito");
+
+
+
+
+
+document.getElementById('password').innerHTML = 'La tua password è '+ nome + cognome + colorepreferito + '19';
+// scrivi a video nomecognomecolorepreferito19 come psw
